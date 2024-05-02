dfe1c57099970e2f2d356a06af3204d886d7c6be
Mooraes <unicomsegundo@gmail.com>
2024-05-02 10:43:47 -0300

    Create a section hero of landing page

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,28 @@
 // importações
 const gulp = require ('gulp');
 const sass = require ('gulp-sass')(require('sass'));
-
+const imagemin = require ('gulp-imagemin'); //instalado uma versão especifica 7.0.1
 
 
 // Criando função
 function styles(){
     return gulp.src('./src/styles/*.scss') //recuperação dos arquivos
-    .pipe(  //para encadear funcoes
-        sass({
-            outputStyle: 'compressed'   //arquivos comprimidos
-        })) 
-    .pipe(
-        gulp.dest(
-            './dist/styles/css'
-        )   
-    )
+    .pipe(sass(
+        {outputStyle: 'compressed'}
+    )) //arquivos comprimidos
+
+    .pipe(gulp.dest('./dist/styles/css'))   //.pipe() --> Serve para encadear funcoes
 }
 
+function images(){
+    return gulp.src('./src/images/**/*') //recuperação dos arquivos
+    .pipe(imagemin()) //arquivos comprimidos
+    .pipe(gulp.dest('./dist/images'))   //.pipe() --> Serve para encadear funcoes
+}
+
+
 // executando função
-exports.default = styles;
+exports.default = gulp.parallel(styles, images);
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles)) //arquivos que serão observados
 }
