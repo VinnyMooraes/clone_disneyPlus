315563ad3a0dec084fcf258afc569f10012f9bd5
Mooraes <unicomsegundo@gmail.com>
2024-05-20 14:16:18 -0300

    minificação dos arquivos js

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,17 @@
 const gulp = require ('gulp');
 const sass = require ('gulp-sass')(require('sass'));
 const imagemin = require ('gulp-imagemin'); //instalado uma versão especifica 7.0.1
-
+const uglify = require('gulp-uglify');
 
 // Criando função
+
+ //minificar os arquivos .js
+function scripts(){    
+    return gulp.src('./src/js/*.js')   //recuperação dos arquivos
+    .pipe(uglify()) //executando o uglify
+    .pipe(gulp.dest('./dist/js'))
+}
+
 function styles(){
     return gulp.src('./src/styles/*.scss') //recuperação dos arquivos
     .pipe(sass(
@@ -21,8 +29,11 @@ function images(){
 }
 
 
+
+
 // executando função
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles)) //arquivos que serão observados
+    gulp.watch('./src/js/*.js', gulp.parallel(scripts)) //observa os arquivos na pasta e executa uma função quando eles são modificados.
 }
